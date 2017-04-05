53bca6153b4c91da41ea6772aee9766b47260a1a
Sliverwing <tlv591208510@gmail.com>
2017-04-05 22:05:00 +0800

    update ExceptionHandler

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -3,19 +3,31 @@
 namespace Sliverwing\ExceptionReporter;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Support\Facades\Mail;
+use \Sliverwing\ExceptionReporter\ExceptionReporter\ExcpetionMailReporter;
+use Illuminate\Foundation\Exceptions\Handler;
 
-class ExceptionHandler extends \App\Exceptions\Handler {
+
+class ExceptionHandler extends Handler{
     
     use DispatchesJobs;
 
     public function report(\Exception $exception)
     {
-        $this->dispatch(new ExceptionReporter(
-            $exception->getFile(),
-            $exception->getCode(),
-            $exception->getMessage(),
-            $exception->getTraceAsString()
-        ));
+        if (config('exception-reporter.mail.enable', false))
+        {
+            $mailTo = config('exception-reporter.mail.to', []);
+            foreach ($mailTo as $to)
+            {
+                Mail::to($to)->queue(new ExcpetionMailReporter(
+                    $exception->getFile(),
+                    $exception->getCode(),
+                    $exception->getMessage(),
+                    $exception->getTraceAsString()
+                ));
+            }
+        }
+        parent::report($exception);
     }
 
 }
