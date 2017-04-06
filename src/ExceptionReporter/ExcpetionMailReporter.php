127545cde1eaa2ac5b0776532c547d963b06b481
Sliverwing <tlv591208510@gmail.com>
2017-04-06 23:19:13 +0800

    use BaseReporter

--- src/ExceptionReporter/ExcpetionMailReporter.php
+++ src/ExceptionReporter/ExcpetionMailReporter.php
@@ -2,35 +2,12 @@
 
 namespace Sliverwing\ExceptionReporter\ExceptionReporter;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ExcpetionMailReporter extends Mailable
 {
-    use Queueable, SerializesModels;
 
-    protected $file;
-    protected $code;
-    protected $message;
-    protected $trace;
-
-
-    /**
-     * ExceptionMailReporter constructor.
-     * @param $file
-     * @param $code
-     * @param $message
-     * @param $trace
-     */
-    public function __construct($file, $code, $message, $trace)
-    {
-        $this->file = $file;
-        $this->code = $code;
-        $this->message = $message;
-        $this->trace  = $trace;
-    }
+    use ExceptionBaseReporter;
 
     /**
      * Build the message.
